97cb52b2d04dcd1695fa91c7ce5ee6fab120910f
Juan Reyes <juanmanuelreyesgarcia816@gmail.com>
2024-10-08 23:58:30 -0500

    create env and start conexión DB

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, sessions, redirect, url_for
+from functools import wraps
+import pymongo
+
+app = Flask(__name__)
+
+app.secret_key="1234"
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
